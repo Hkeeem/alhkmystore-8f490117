0269a1b51f4a0ba3b99f9168ce8524d3f29a88d7
hkeeem <Hkeeem@users.noreply.github.com>
2026-09-13 00:32:53 +0300

    Import fetchStorePrices and fetchActiveCoupons

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,5 @@
+import { fetchStorePrices, fetchActiveCoupons } from "@/integrations/hkeeem-prices";
+
 import { createFileRoute, Link, useNavigate } from "@tanstack/react-router";
 import { useState } from "react";
 import { bestDeals, comparableGroups, stores, getStore } from "@/data/deals";
